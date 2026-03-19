1d5ba422f83972689685b2acb31dd834ab3d0b3f
oxy6enn <oxy6en@hotmail.com>
2026-03-20 00:25:54 +0700

    chore: add Next.js day 1 learning files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/app/(landing)/Footer";
-import Navbar from "@/app/(landing)/Navbar";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,9 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         {children}
-        <Footer />
       </body>
     </html>
   );
